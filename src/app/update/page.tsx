e8842526471c0a49fda5a00030f059ae2f23739a
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-11-29 23:39:27 +0300

    add cupdate route

---
+++ src/app/update/page.tsx
@@ -0,0 +1,11 @@
+import UpdatePost from "../_lib/UpdatePost"
+
+const UpdatePostPage = () => {
+    return (
+      <>
+        <UpdatePost />
+      </>
+    )
+  }
+  
+  export default UpdatePostPage
