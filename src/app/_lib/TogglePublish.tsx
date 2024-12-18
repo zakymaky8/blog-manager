c7eef00dfb5d977a11a48d526c590de3c991901e
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-19 00:45:51 +0300

    toggle publish

--- src/app/_lib/TogglePublish.tsx
+++ src/app/_lib/TogglePublish.tsx
@@ -1,13 +1,46 @@
+"use client"
 
+import { usePathname, useRouter } from "next/navigation"
+import { FormEvent } from "react"
 
 const TogglePublish = ({postId, action}: {
     postId: string,
     action: string
     }) => {
+        const router = useRouter();
+        const pathname = usePathname()
 
+        async function handleSubmit(e: FormEvent<HTMLElement>) {
+            e.preventDefault()
+            const getTokenFromCookies = () => {
+                const cookies = document.cookie.split("; ");
+                const tokenCookie = cookies.find(cookie => cookie.startsWith("token="));
+                return tokenCookie ? tokenCookie.split("=")[1] : null;
+            };
+            const token = getTokenFromCookies();
+            try {
+                const res = await fetch(`http://localhost:3456/posts/${postId}/${action.toLowerCase()}?_method=PUT`, {
+                    method: "PUT",
+                    headers: {
+                        "authorization": `Bearer ${token}`
+                    }
+                })
+                if (!res.ok) {
+                    router.replace("/admin-login")
+                } else {
+                    router.replace(pathname)
+                }
+            } catch {
+                throw new Error("falied to published")
+            }
+    }
     return (
-        <form action={`http://localhost:3456/posts/${postId}/${action.toLowerCase()}?_method=PUT`} method='POST'>
-            <button className="text-[11px] w-fit h-fit p-1" type='submit'>{action}</button>
+        <form onSubmit={handleSubmit}>
+            <button
+                className="text-[11px] w-fit h-fit p-1"
+                type='submit'>
+                    {action}
+            </button>
         </form>
     )
 }
