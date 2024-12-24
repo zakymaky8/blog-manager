e544fe30def604788d68edf3276f3c74c6778320
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-24 14:29:26 +0300

    update comment card to have special styles

--- src/app/_lib/CommentCard.tsx
+++ src/app/_lib/CommentCard.tsx
@@ -1,39 +1,100 @@
-import DeleteButton from "./DeleteButton";
+import { cookies } from "next/headers";
+// import DeleteButton from "./DeleteButton";
 // import LikeButton from "./LikeButton";
-import ReplyButton from "./ReplyButton";
+// import ReplyButton from "./ReplyButton";
+import { redirect } from "next/navigation";
+
+// import Image from "next/image";
+// import editBtn from "../../../public/edit_icon.svg"
+// import Link from "next/link";
+import SingleComment from "./SingleComment";
+import Replies from "./Replies";
+
 
 type TProps = {
     postId: string
 }
 
-type TComment = {
+export type TAuthor = {
+    users_id: string,
+    firstname: string,
+    lastname: string,
+    username: string,
+    password: string,
+    Role: string
+}
+
+export type TComment = {
     content: string;
     createdAt: Date;
     lastUpdate: Date;
-    likes: number;
+    likes: string[];
     user_id: string;
     comments_id: string;
     post_id: string;
 }
 
-const CommentsCard = async ({postId}: TProps) => {
-    const res = await  fetch(`http://localhost:3456/posts/${postId}/comments`);
-    const { comments }: {comments: TComment[]} = await res.json();
+export type TReply = {
+  content: string;
+  user_id: string;
+  replies_id: string;
+  comment_id: string;
+  replied_id: string;
+  likes: string[]
+}
+
+export type TReplyActor = {
+  replier: TAuthor,
+  replied_to: TAuthor
+}
+
+const CommentsCard = async ({ postId }: TProps) => {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+    const res = await  fetch(`http://localhost:3456/posts/${postId}/comments`,{
+      headers: {
+        "authorization": `Bearer ${token}`,
+        "content-type": "application/json"
+      }
+    });
+    if (!res.ok) {
+      redirect("/login")
+    }
+    const { data } = await res.json();
+    const [comments, authors, currentUser, replies, replyActorPairs]: [TComment[], TAuthor[], TAuthor, TReply[], TReplyActor[]] = data;
+
+    function findAuthor(id:string) {
+      const authr = authors.find((author) => author.users_id === id);
+      return authr
+    }
+
 
   return (
-    <div>
+    <div className="bg-slate-200 p-3 rounded-xl max-w-[450px] min-w-[350px]">
+      <h2 className="font-bold text-xl mb-5">Comments: {comments.length}</h2>
         {comments.map((comment) => {
+            const commentIsLiked = comment.likes.includes(currentUser.users_id) ? true : false;
+            const commentAuthor = findAuthor(comment.user_id);
+            const thisCommentReply = replies.filter(reply => reply.comment_id === comment.comments_id) || null;
             return (
                 <div key={comment.comments_id} className="flex flex-col gap-3">
-                <div className="flex flex-col  p-3 rounded-xl " style={{maxWidth: "550px"}}>
-                  <h1 className="text-sm text-gray-600 hover:underline cursor-pointer">@zakymak</h1>
-                  <p className="pt-1 text-xs italic">{comment.content}</p>
-                  <div className="self-end flex gap-2 items-start">
-                    {/* <LikeButton /> */}
-                    <ReplyButton />
-                    <DeleteButton postId={postId} commentId={comment.comments_id}/>
-                  </div>
-                </div>
+                  <SingleComment
+                    comment={comment}
+                    commentAuthor={commentAuthor}
+                    commentIsLiked={commentIsLiked}
+                    currentUser={currentUser}
+                    authorname = {findAuthor(comment.user_id)}
+                    postId= {postId}
+                  />
+                  <Replies
+                    currentUser={currentUser}
+                    postId={postId}
+                    comment={comment}
+                    replyActorPairs={replyActorPairs}
+                    commentReply={thisCommentReply} />
+
+                  <hr className="border-[1px] border-black opacity-20"/>
+
               </div>
             )
         })}
