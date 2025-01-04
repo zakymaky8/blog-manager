6539a78591e40570e8e88a10b745960b7bb5ac08
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-04 16:34:10 +0300

    modify update post section

--- src/app/_lib/UpdatePost.tsx
+++ src/app/_lib/UpdatePost.tsx
@@ -1,66 +1,53 @@
+import Link from "next/link";
 import Search from "./Search"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-const UpdatePost = () => {
+export type TPost = {
+    posts_id: string;
+    title: string;
+    content: string;
+    createdAt: Date;
+    lastUpdate: Date;
+    postImgs: string[];
+    likes: number;
+    views: number;
+    user_id: string;
+    status: "DRAFT" | "PUBLISHED";
+}
+
+const UpdatePost = async () => {
+    const cookieStore =  cookies();
+    const token = (await cookieStore).get("token")?.value;
+    console.log(token);
+    
+    const res = await fetch(`http://localhost:3456/posts`, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    });
+    if (!res.ok) {
+        redirect("/admin-login")
+    }
+    const { posts } = await res.json()
   return (
     <div className="w-full flex flex-col flex-auto items-center gap-4 p-4 text-black">
-        <h1>Update Post</h1>
+        <h1 className="text-2xl pb-2 border-b-[1px] border-black">Update Published Post</h1>
         <Search />
         <div className="flex flex-col gap-3">
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. my life really such to be honest ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
-
-            <div className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
-                <h1 className="max-w-[60%] text-xs">1. Title Title Title Title Title Title Title ... </h1>
-                <div className="flex gap-2">
-                    <button className="h-[22px] p-1 text-xs">Update</button>
-                </div>
-            </div>
+            {
+                posts.map((post:TPost, index: number) => {
+                    return (
+                        <div key={post.posts_id} className="bg-slate-400 flex justify-between min-w-96 border-[1px] border-black p-3 items-center gap-5 rounded-md">
+                            <h1 className="max-w-[60%] text-xs">{index+1 + ". " + (post.title.length > 50 ? post.title.slice(0, 50) + "..." : post.title)}</h1>
+                            <div className="flex gap-2">
+                                <Link href={`/update/${post.posts_id}`}><button className="h-[22px] p-1 text-xs">Update</button></Link>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     </div>
   )
